d1ea881e9d2edec4eeb37a5decc806ed4ff2c3bb
Skyler Dowdy <skyler2440@gmail.com>
2019-05-01 18:46:41 -0400

    stylz

--- instagram/src/components/PostContainer/PostCnt.js
+++ instagram/src/components/PostContainer/PostCnt.js
@@ -5,10 +5,23 @@
 // const PostsContainer = props => {
 //         console.log('Post Cont Props', props)
 //     return (
-//         <div className='post-cnt-wrapper'>
-//         {props.post.(p => <Post key={p.imageUrl} post={p} />)}
-//         </div>
+        // <div className='post-cnt-wrapper'>
+        //  {props.post.(p => <Post key={p.imageUrl} post={p} />)} 
+        //  </div>
 //     );
 // };
 
 // export default PostsContainer;
+import React from 'react';
+import Post from './Post';
+import './Post.css';
+
+const PostsContainer = props => {
+  return (
+    <div className="post-cont-wrapper">
+      {props.posts.map(p => <Post key={p.imageUrl} post={p} />)}
+    </div>
+  );
+};
+
+export default PostsContainer;
