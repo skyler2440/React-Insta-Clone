41ec026a385932173e8cde7d3dcad385299b717b
Skyler Dowdy <skyler2440@gmail.com>
2019-05-02 16:45:37 -0400

    added authenticate for login

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,60 +1,27 @@
 import React from 'react';
-import dummyData from './dummy-data';
+// import dummyData from './dummy-data';
 import './App.css';
-import SearchBar from './components/SearchBar/SBC';
-import PostsContainer from './components/PostContainer/PostCnt';
-import Post from './components/PostContainer/Post';
-class App extends React.Component{
-  constructor(){
+// import SearchBar from './components/SearchBar/SBC';
+import PostsPage from './components/PostContainer/PostPage';
+import LoginPage from './components/Login/LoginPage';
+import withAuthenticate from './components/Authentication/withAuthenticate';
+// import Post from './components/PostContainer/Post';
+class App extends React.Component {
+  constructor() {
     super();
-    this.state= {
-      post: [],
-      username: ''
-    };
+    this.state = {};
   }
-componentDidMount() {
-  this.setState({post: dummyData})
-}
-// searchPostHandler = e => {
-//   const post = this.state.posts.filter(p => {
-//     if (p.username.includes(e.target.value)) {
-//       return p;
-//     }
-//   });
-//   this.setState({filteredPost: post});
-// };
-searchPostHandler = () => {
-  if (this.state.username) {
-    return this.state.post.filter(post => {
-      return post.username.includes(this.state.username)
-    });
+
+  render() {
+    return (
+      <div className="App">
+        <ComponentFromWithAuthenticate />
+      </div>
+    );
   }
-  return this.state.post
-}
-changeHandler = event =>{
-  this.setState({username: event.target.value });
-};
-render() {
-  const filteredPost = this.searchPostHandler();
-  return (
-    <div className="App">
-     <SearchBar 
-    //  searchPostHandler={this.searchPostHandler}
-    post={this.state.username}
-     changeHandler={this.changeHandler}
-     />
-     <div>
-     {
-            filteredPost && filteredPost.map((post) => {
-              return <Post
-                key={post.timestamp}
-                post={post} />
-            })
-          }
-     </div>
-    </div>
-  );
-}
 }
 
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage)(LoginPage);
+
+// export default App;
 export default App;
